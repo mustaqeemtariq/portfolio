ed3bbbbfa50e2f1ee2e9354eeda00aee60a68655
Mustaqeem Tariq <mustaqeemtariq66@gmail.com>
2023-04-07 18:05:32 +0500

    add app layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { AppHeader } from './header'
+import { Sidebar } from './sidebar'
+
+interface AppLayoutProps {
+    title?: string
+    renderHeader?: boolean
+    renderSidebar?: boolean
+    docTitle?: string,
+    children: React.ReactNode
+}
+
+export const AppLayout = ({children, title, renderHeader, renderSidebar}: AppLayoutProps) => {
+  const docTitleText = title ? `${title} | Portfolio` : 'Portfolio'
+  return (
+    <>
+      {renderHeader ? <AppHeader /> : null}
+      {renderSidebar ? <Sidebar>{children}</Sidebar> : <>{children}</>}
+    </>
+  )
+}
---
+++ src/app/sidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface SidebarProps {
+  children: React.ReactNode
+}
+
+export const Sidebar = ({ children }: SidebarProps) => {
+  return (
+    <div>sidebar</div>
+  )
+}
